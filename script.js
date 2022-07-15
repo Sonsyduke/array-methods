0aa85fde1daaf1e940efe132a3171a597d5540c2
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-15 14:28:36 +0200

    Reduce method exercise

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-const marvelMovies = [{}];
+const marvelMovies = [
+  {
+    name: "Eternals",
+    year: 2021,
+    rating: 47,
+  },
+
+  {
+    name: "Black Widow",
+    year: 2021,
+    rating: 79,
+  },
+
+  {
+    name: "Thor: Ragnarok",
+    year: 2017,
+    rating: 93,
+  },
+
+  {
+    name: "Avengers: Infinity War",
+    year: 2018,
+    rating: 85,
+  },
+
+  {
+    name: "Spider-Man: Homecoming",
+    year: 2017,
+    rating: 92,
+  },
+];
+
+const highestRatedMUMovie = marvelMovies.reduce(
+  (highestRated, currentRated) => {
+    if (currentRated.rating > highestRated.rating) {
+      return currentRated;
+    }
+    return highestRated;
+  }
+);
